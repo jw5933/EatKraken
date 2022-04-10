82d667c16b91636628c517fde0227d2afefc9838
jw5933 <jw5933@nyu.edu>
2022-04-10 16:25:49 -0400

    Ingredient spawner
    Can put ingredient back into its spawner

---
+++ Assets/Scripts/Cooking System/Ingredients/IngredientSpawner.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IngredientSpawner : MonoBehaviour
+{
+    [SerializeField] private GameObject myIngredientPrefab;
+    string myIngredientName;
+
+    private Player player;
+    private GameObject newIngredient;
+
+    private void Awake(){
+        this.transform.SetParent(FindObjectOfType<GameManager>().ingredientParent);
+        player = FindObjectOfType<Player>();
+        myIngredientName = myIngredientPrefab.name;
+    }
+
+    private void OnMouseDown(){
+        if (!player.handFree){
+            CheckDespawn();
+            return;
+        }
+        SpawnIngredient();
+        player.PickUpItem(newIngredient);
+    }
+
+    private void CheckDespawn(){
+        if (player.holdingIngredient && player.ingredient.name == myIngredientName){
+            Destroy(player.DropItem("ingredient"));
+        }
+    }
+
+    private void SpawnIngredient(){
+        newIngredient = Instantiate(myIngredientPrefab, this.transform);
+        newIngredient.transform.position = transform.position;
+        newIngredient.GetComponent<Ingredient>().parent = this.transform;
+    }
+}
